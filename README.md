d445484406ec9bd00d98e1dcc817771874fef47c
Tomáš Milata <tomas@faculty.ai>
2022-01-20 16:38:52 +0100

    Cleanup

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Links:
 
 - [Data](https://github.com/Kaushik-Varma/linear_regression_model_python/blob/main/Company_data.csv)
 
-- [Linear regression chart using Altair]( https://altair-viz.github.io/user_guide/transform/regression.html)
+- [Linear regression chart using Altair]( https://altair-viz.github.io/user_guide/transform/regression.html). This demo
+  uses a modified example from this article.
--- app-predict/app.py
+++ app-predict/app.py
@@ -3,48 +3,48 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash
-import dash_bootstrap_components as dbc
 
 import mlflow
 import faculty_models
 
 
+app = dash.Dash(__name__)
 
-app = dash.Dash(
-    __name__,
-    external_stylesheets=[dbc.themes.BOOTSTRAP]
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="Predict Sales"),
+        html.Label("Spent on TV ads: "),
+        dcc.Input(id="input-div"),
+        html.Div(id="output-div", children=[]),
+    ]
 )
 
-
-app.layout = html.Div(children=[
-    html.H1(children='Predict Sales'),
-    html.Label('Spent on TV ads: '),
-    dcc.Input(id='input-div'),
-    html.Div(id='output-div', children=[])
-])
-
-
-model = faculty_models.load_mlmodel(
-    project_id="d5090325-0842-45dc-a96c-4d1e23668d22",
-    model_id="fed0a51c-a7d8-4ab9-89e9-6c46e4ae77d7"
-)
+# CHANGE ME: Copy-paste the code snippet from the model registry below.
+#
+# Something like:
+#   model = faculty_models.load_mlmodel(
+#         project_id="YOUR PROJECT ID",
+#         model_id="YOUR MODEL ID"
+#   )
 
 
 @app.callback(
-        Output(component_id='output-div', component_property='children'),
-        [Input(component_id='input-div', component_property='value')]
+    Output(component_id="output-div", component_property="children"),
+    [Input(component_id="input-div", component_property="value")],
 )
 def update_output(input_value):
     if input_value is None or not input_value:
-        return ['Missing input']
+        return ["Missing input"]
     try:
         spent_on_tv = int(input_value)
     except ValueError:
-        return ['Invalid input']
+        return ["Invalid input"]
     model_input = [[spent_on_tv]]
     [predicted_sales] = model.predict(model_input)
     predicted_sales
     return [f"Predicted sales: {predicted_sales} $"]
 
-if __name__ == '__main__':
-    app.run_server(host='127.0.0.1', port=8888, debug=True)
+
+if __name__ == "__main__":
+    app.run_server(host="127.0.0.1", port=8888, debug=True)
